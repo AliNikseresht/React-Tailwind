e4282463edf7cf68936fbc0e9089092cb18077ec
Ali <120955025+AliNikseresht@users.noreply.github.com>
2023-06-06 23:01:47 +0330

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./component/header/header";
+import Section from "./component/content/content";
+import Footer from "./component/footer/footer";
+import './index.css'
+
+
+
+export default function Main() {
+  return (
+    <main>
+      <Header />
+      <Section />
+      <Footer />
+    </main>
+  )
+}
